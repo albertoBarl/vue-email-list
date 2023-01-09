8971815b210d7637db12e17dd10723f63d5fa32e
albertoBarl <alberto.barletta00@gmail.com>
2023-01-09 17:15:46 +0100

    added html content, linked function, BONUS done

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ createApp({
     createEmail() {
       for (let i = 0; i < 10; i++) {
         axios
-          .get("https://flynn.boolean.careers/exercises/api/random/mail%27")
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
           .then((genEmail) => {
             this.email = genEmail.data.response;
             this.emailArray.push(this.email);
